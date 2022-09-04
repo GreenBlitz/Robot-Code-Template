46c2dc7b92e481a81b29b81234f03be869ddc320
itamaroryan <itamar.oryan@gmail.com>
2022-09-04 10:16:40 +0300

    asaf - remade template from rapid-react code

---
+++ src/main/java/edu/greenblitz/pegasus/RobotMap.java
@@ -0,0 +1,13 @@
+package edu.greenblitz.pegasus;
+
+
+
+public class RobotMap {
+	public static class Robot {
+		public static class Joystick {
+			public static final int MAIN = 0;
+			public static final int SECOND = 1;
+		}
+
+	}
+}
